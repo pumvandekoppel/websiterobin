aba8b02d10e42f454be414b325bc1ae0c47251bc
Pum van de Koppel <pumvandekoppel@gmail.com>
2024-08-24 00:18:05 +0200

    klaaro

--- scaleIndex.js
+++ scaleIndex.js
@@ -2,8 +2,8 @@ var scaleRatioRock = 220; // if a div is 218.075px wide, the horizontal scale of
 var scaleRatioGips = 125.5; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
 var scaleRatioFountain = 114.3; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
 var scaleRatioLobsters = 581; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
-var scaleRatioBlock = 83.75; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
-var scaleRatioBooks = 102.075; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
+// var scaleRatioBlock = 83.75; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
+var scaleRatioBooks = 102; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
 var scaleRatioPaper = 133; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
 var scaleRatioPaintings = 80; // if a div is 218.075px wide, the horizontal scale of the text is 1 | 436.15 -> 2 etc
 var scaleRatioAbout;
@@ -17,7 +17,7 @@ function scaleIndexToWidth() {
     var newScaleXGips = width / scaleRatioGips
     var newScaleXFountain = width / scaleRatioFountain
     var newScaleXLobsters = width / scaleRatioLobsters
-    var newScaleXBlock = width / scaleRatioBlock
+    // var newScaleXBlock = width / scaleRatioBlock
     var newScaleXBooks = width / scaleRatioBooks
     var newScaleXPaper = width / scaleRatioPaper
     var newScaleXPaintings = width / scaleRatioPaintings
@@ -26,10 +26,10 @@ function scaleIndexToWidth() {
     bigTitles[1].style.transform = "scaleX(" + newScaleXGips +")"
     bigTitles[2].style.transform = "scaleX(" + newScaleXFountain+")"
     bigTitles[3].style.transform = "scaleX(" + newScaleXLobsters +")"
-    bigTitles[4].style.transform = "scaleX(" + newScaleXBlock +")"
-    bigTitles[5].style.transform = "scaleX(" + newScaleXBooks +")"
-    bigTitles[6].style.transform = "scaleX(" + newScaleXPaper +")"
-    bigTitles[7].style.transform = "scaleX(" + newScaleXPaintings +")"
+    // bigTitles[4].style.transform = "scaleX(" + newScaleXBlock +")"
+    bigTitles[4].style.transform = "scaleX(" + newScaleXBooks +")"
+    bigTitles[5].style.transform = "scaleX(" + newScaleXPaper +")"
+    bigTitles[6].style.transform = "scaleX(" + newScaleXPaintings +")"
 }
 
 scaleIndexToWidth();
