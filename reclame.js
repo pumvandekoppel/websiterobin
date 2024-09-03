e2ca1b9c79505db881ac78586b790e98b46ce540
Pum van de Koppel <pumvandekoppel@gmail.com>
2024-09-03 16:17:27 +0200

    yoyo

--- reclame.js
+++ reclame.js
@@ -1,13 +1,13 @@
-var hidden = new Boolean(true);
+var hidden = new Boolean(false);
 
 function reclame() {
     if (hidden == true) {
         document.getElementById("plug").style.display = "none";
-        document.getElementsByClassName("self-promotion_hidden")[0].style.display = "inline";
+        document.getElementsByClassName("self-promotion_hidden")[0].style.display = "inline-block";
         hidden = false
     }
     else {
-        document.getElementById("plug").style.display = "inline";
+        document.getElementById("plug").style.display = "inline-block";
         document.getElementsByClassName("self-promotion_hidden")[0].style.display= "none";
         hidden = true
     }
