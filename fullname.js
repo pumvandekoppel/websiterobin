79c93994d6496de12914c92d35268c5f79aca9f9
Pum van de Koppel <pumvandekoppel@gmail.com>
2026-08-07 16:28:06 +0200

    responsive js full name

--- fullname.js
+++ fullname.js
@@ -1,8 +1,9 @@
+const mediaQueryForName = window.matchMedia( '(min-width: 412px)' )
 var fullname = false
 const middleName = document.getElementById("middle-name")
 
 function toggleFullName() {
-    if (fullname == false) {
+    if (fullname == false && mediaQueryForName) {
         middleName.style.display = "initial"
         fullname = true
     }
